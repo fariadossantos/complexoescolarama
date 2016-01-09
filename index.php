bf3dfe0114314bace5b47ef0ae178c77d4f93bb0
fariaquitamba <fariaquitamba5@gmail.com>
2016-01-09 13:50:22 +0100

    Implementação dos chat para secretaria

--- index.php
+++ index.php
@@ -5,9 +5,13 @@
 		<title>Complexo Escolar AMA</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0" />
 		<link rel="shortcut icon"  href=img/favicon.ico sizes="16x16" type="image/x-icon">
+		<!-- Start of complexoescolarama Zendesk Widget script -->
+<script>/*<![CDATA[*/window.zEmbed||function(e,t){var n,o,d,i,s,a=[],r=document.createElement("iframe");window.zEmbed=function(){a.push(arguments)},window.zE=window.zE||window.zEmbed,r.src="javascript:false",r.title="",r.role="presentation",(r.frameElement||r).style.cssText="display: none",d=document.getElementsByTagName("script"),d=d[d.length-1],d.parentNode.insertBefore(r,d),i=r.contentWindow,s=i.document;try{o=s}catch(c){n=document.domain,r.src='javascript:var d=document.open();d.domain="'+n+'";void(0);',o=s}o.open()._l=function(){var o=this.createElement("script");n&&(this.domain=n),o.id="js-iframe-async",o.src=e,this.t=+new Date,this.zendeskHost=t,this.zEQueue=a,this.body.appendChild(o)},o.write('<body onload="document._l();">'),o.close()}("https://assets.zendesk.com/embeddable_framework/main.js","complexoescolarama.zendesk.com");/*]]>*/</script>
+<!-- End of complexoescolarama Zendesk Widget script -->
 		<?php require_once('util/import.php') ?>
 	</head>
 	<body id="voltarTopo">
+
 		<section class="header pull-left animated wow fadeInLeft " id="class">
 			<div class="logo pull-left" id="logo"><img src="img/logo.png" alt="Logotipo" /></div>
 			<p class=" animated wow fadeInLeft subscreva" data-wow-delay=".2s" id="subscreva"> <a href="#subscreva"><i class="fa fa-pencil"></i>Inscreva-se já</a></p>
