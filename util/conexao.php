1175cbddb3cbe54c576c0c9739059747cc4cd037
fariaquitamba <fariaquitamba5@gmail.com>
2016-01-15 03:12:13 +0100

    Implementações da nova pagina de conexão

--- util/conexao.php
+++ util/conexao.php
@@ -4,7 +4,7 @@ define('USER', 'u734996712_cea');
 define('PASSWORD', '1q2w3e4r5t6y');
 define('BANCOSELECT', 'u734996712_cea');
 
-$conexao = mysql_connect(HOST, USER, PASSWORD) or die('1');
+$conexao = mysqli_connect(HOST, USER, PASSWORD) or die('1');
 $selectdb = mysql_select_db(BANCOSELECT, $conexao);
 
 
