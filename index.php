b1e6598dc946e53d3ae65caa895a66c89fa58f4d
fariaquitamba <fariaquitamba5@gmail.com>
2016-01-15 14:22:24 +0100

    inserção do zopim a pagina

--- index.php
+++ index.php
@@ -12,9 +12,9 @@ require_once('model/funcoes.php');
 		<link rel="shortcut icon"  href=img/favicon.ico sizes="16x16" type="image/x-icon">
 		<?php require_once('util/import.php') ?>
 		<!-- Start of complexoescolarama Zendesk Widget script -->
-	<script>/*<![CDATA[*/window.zEmbed||function(e,t){var n,o,d,i,s,a=[],r=document.createElement("iframe");window.zEmbed=function(){a.push(arguments)},window.zE=window.zE||window.zEmbed,r.src="javascript:false",r.title="",r.role="presentation",(r.frameElement||r).style.cssText="display: none",d=document.getElementsByTagName("script"),d=d[d.length-1],d.parentNode.insertBefore(r,d),i=r.contentWindow,s=i.document;try{o=s}catch(c){n=document.domain,r.src='javascript:var d=document.open();d.domain="'+n+'";void(0);',o=s}o.open()._l=function(){var o=this.createElement("script");n&&(this.domain=n),o.id="js-iframe-async",o.src=e,this.t=+new Date,this.zendeskHost=t,this.zEQueue=a,this.body.appendChild(o)},o.write('<body onload="document._l();">'),o.close()}("//assets.zendesk.com/embeddable_framework/main.js","complexoescolarama.zendesk.com");
-	/*]]>*/</script>
-	<!-- End of complexoescolarama Zendesk Widget script -->
+<script>/*<![CDATA[*/window.zEmbed||function(e,t){var n,o,d,i,s,a=[],r=document.createElement("iframe");window.zEmbed=function(){a.push(arguments)},window.zE=window.zE||window.zEmbed,r.src="javascript:false",r.title="",r.role="presentation",(r.frameElement||r).style.cssText="display: none",d=document.getElementsByTagName("script"),d=d[d.length-1],d.parentNode.insertBefore(r,d),i=r.contentWindow,s=i.document;try{o=s}catch(c){n=document.domain,r.src='javascript:var d=document.open();d.domain="'+n+'";void(0);',o=s}o.open()._l=function(){var o=this.createElement("script");n&&(this.domain=n),o.id="js-iframe-async",o.src=e,this.t=+new Date,this.zendeskHost=t,this.zEQueue=a,this.body.appendChild(o)},o.write('<body onload="document._l();">'),o.close()}("//assets.zendesk.com/embeddable_framework/main.js","complexoescolarama.zendesk.com");
+/*]]>*/</script>
+<!-- End of complexoescolarama Zendesk Widget script -->
 	</head>
 	<body id="voltarTopo">
 
