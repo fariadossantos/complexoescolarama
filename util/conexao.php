87463b096d97fe4a1e8aca5f0425e55afb794267
fariaquitamba <fariaquitamba5@gmail.com>
2016-01-31 18:44:56 +0100

    Implementação do Slider

--- util/conexao.php
+++ util/conexao.php
@@ -1,13 +1,13 @@
 <?php
-/*define('HOST', 'localhost');
+define('HOST', 'localhost');
 define('USER', 'u734996712_cea');
 define('PASSWORD', '1q2w3e4r5t6y');
 define('BANCOSELECT', 'u734996712_cea');
 
 $conexao = mysqli_connect(HOST, USER, PASSWORD) or die('1');
-$selectdb = mysqli_select_db($conexao, BANCOSELECT);*/
+$selectdb = mysqli_select_db($conexao, BANCOSELECT);
 
-define('HOST', 'localhost');
+/*define('HOST', 'localhost');
 define('USER', 'root');
 define('PASSWORD', '');
 define('BANCOSELECT', 'complexoescolarama');
@@ -23,5 +23,5 @@ $selectdb = mysqli_select_db($conexao, BANCOSELECT);
 
 //print_r($resutaldo);
 
-
+*/
  ?>
